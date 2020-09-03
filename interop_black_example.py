c161a478ce80b891b38c86f5e2b6fd5d747ec72a
Josh VanDeraa <josh.vanderaa@networktocode.com>
2020-09-02 21:46:53 -0500

    Interop black code style format

---
+++ interop_black_example.py
@@ -0,0 +1,7 @@
+def main():
+    """Main code demo."""
+    print('Hello Interop2020')
+    interop_dict = {"event_month": "October", "event_dates": "5-6", "event_year": 2020, "summit": "Network Automation Summit"}
+
+if __name__ == "__main__":
+    main()
