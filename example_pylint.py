f73f6f525f5867991f9a203952dc1855ae4868c5
Josh VanDeraa <josh.vanderaa@networktocode.com>
2021-02-11 10:24:18 -0600

    Renames pylint

--- example_pylint.py
+++ example_pylint.py
@@ -3,11 +3,12 @@ import sys
 
 
 def main():
-    """Main code execution."""
     session = requests.Session()
     session.get("https://www.networktocode.com")
+    session.get("https://josh-v.com")
 
     print(session.headers)
+
 
 if __name__ == "__main__":
     main()
