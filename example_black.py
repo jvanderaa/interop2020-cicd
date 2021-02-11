1711d6deff1541dacb63bc7a0ff2efea9948eec6
Josh VanDeraa <josh.vanderaa@networktocode.com>
2021-02-11 10:22:00 -0600

    Updates file contents

--- example_black.py
+++ example_black.py
@@ -1,7 +1,7 @@
 def main():
     """Main code demo."""
-    print('Hello Interop2020')
-    interop_dict = {"event_month": "October", "event_dates": "5-6", "event_year": 2020, "summit": "Network Automation Summit"}
+    print('Hello CI Demo!')
+    example_dictionary = {"event_month": "March", "event_dates": "17-20", "event_year": 2021, "example": "Network Automation Example"}
 
 if __name__ == "__main__":
     main()
